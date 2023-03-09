a9458b57f5a80d983a29c41184646a144de62349
kasunkavinda <kasunkavinda.bc@gmail.com>
2023-03-09 23:21:24 +0530

    redux tool kit adding

--- kasuns_movie_corner/src/app/page.tsx
+++ kasuns_movie_corner/src/app/page.tsx
@@ -1,14 +1,20 @@
 import NavBar from "@/components/navBar/page";
 import React from "react";
 import MovieGetter from "./movieGetter/page";
+import { Counter } from "@/components/counter/page";
+import { store } from '../store/store'
+import { Provider } from 'react-redux'
 
 const page = () => {
   console.log("third");
   return (
     <div>
+      <Provider store={store}>
       <NavBar />
       {/* @ts-expect-error Server Component */}
       <MovieGetter />
+      <Counter />
+      </Provider>
     </div>
   );
 };
